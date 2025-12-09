0a533af908e167e22fc01d84dd3d4263b1bda907
John Gallagher <jmg2016@gmail.com>
2025-12-08 22:33:10 -0500

    2020 update

--- countdown.py
+++ countdown.py
@@ -1,12 +1,19 @@
-import sys
 import argparse
-import time
-from rgbmatrix import RGBMatrix, RGBMatrixOptions
-from rgbmatrix.graphics import Color, DrawText, Font
+import logging
 import mta
+import sys
+import time
 import weather
 
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from rgbmatrix.graphics import Color, DrawText, Font, DrawLine
+
+logger = logging.getLogger(__name__)
+
 COLORS = {
+    'white': Color(255, 255, 255),
+    'blue': Color(0, 0, 255),
+    'black': Color(0, 0, 0),
     'green': Color(0, 255, 0),
     'red': Color(255, 0, 0),
     'yellow': Color(255, 255, 0),
@@ -15,6 +22,7 @@ COLORS = {
 
 GLYPH_COLORS = {
     'Q': COLORS['yellow'],
+    'M': COLORS['orange'],
     '4': COLORS['green'],
     '5': COLORS['green'],
     '6': COLORS['green'],
@@ -28,7 +36,18 @@ STATUS_COLORS = {
 }
 
 SUBWAYTIME_STOPS = [('Q/Q04', 'Downtown'), ('5/626', 'Downtown')]
-LINES = ['4', '5', '6', 'Q']
+SLOTS = ['6', '4|5', 'Q|M']
+
+class StatusBar:
+    def __init__(self, height, color=None):
+        self.height = height
+        self.color = color
+        if not color:
+            self.color = COLORS['green']
+
+    def draw(self, matrix, x, y):
+        DrawLine(matrix, x, y, x, y - self.height, self.color)
+        return 1
 
 class StatusText:
     def __init__(self, font, text, color=None):
@@ -92,49 +111,70 @@ def render_loop(glyphs, font):
                 status = mta.status()
                 times_by_line.update(mta.times_from_subwaytime(SUBWAYTIME_STOPS))
                 last_mta_fetch = now
-                err = None
             except Exception, ex:
-                err = str(ex)
-
+                logger.exception('failed to get time')
 
         if now - last_weather_fetch > 300:
             try:
                 current = weather.current()
-                forecast = weather.forecast()
+                forecast = weather.forecast_accuweather()
                 last_weather_fetch = now
             except Exception, ex:
-                err = str(ex)
+                logger.exception('failed to get time')
 
 
         status_lines = []
         # always display the lines in order
-        for line in LINES:
-            mins = []
-            for stop_time in times_by_line.get(line, []):
-                if stop_time - now < 60:
-                    continue
-                mins.append(str(int((stop_time - now) / 60)))
-                if len(mins) == 2:
-                    break
-            if mins:
+        for slot in SLOTS:
+            slot_entries = []
+            slot_lines = slot.split('|')
+            for line in slot_lines:
+                for stop_time in times_by_line.get(line, []):
+                    if stop_time - now < 60:
+                        continue
+                    slot_entries.append((int((stop_time - now) / 60.0), line))
+            slot_entries = sorted(slot_entries)[0:2]
+            mins = [str(e[0]) for e in slot_entries]
+            seen = set()
+            lines = [e[1] for e in slot_entries if not (e[1] in seen or seen.add(e[1]))]
+            if not lines:
+                lines = [slot_lines[0]]
+
+            status_line = []
+            for line in lines:
                 line_status = status.get(line, 'GOOD SERVICE')
-                status_lines.append(StatusLine(
-                    [
-                        StatusText(glyphs, '|', STATUS_COLORS.get(line_status, COLORS['green'])),
-                        StatusText(glyphs, line, GLYPH_COLORS.get(line, COLORS['green'])),
-                    ],
-                    StatusText(font, ','.join(mins) + 'm')
-                ))
+                status_line.append(StatusText(glyphs, '|', STATUS_COLORS.get(line_status, COLORS['green'])))
+                status_line.append(StatusText(glyphs, line, GLYPH_COLORS.get(line, COLORS['green'])))
+            status_lines.append(StatusLine(
+                status_line,
+                StatusText(font, ','.join(mins) + 'm' if mins else '?')
+            ))
+
 
         if current:
-            status_lines.append(StatusLine([
+            status_line = [
+                StatusText(glyphs, '|', COLORS['black']),
                 StatusText(font, str(current['temp'])),
                 StatusText(glyphs, 'o')
-            ]))
-
-        if err:
-            status_lines.append(StatusLine([StatusText(font, err)]))
-
+            ]
+            if forecast:
+                forecast = forecast[0:24]
+                high = max([hr['temp'] for hr in forecast])
+                low = min([hr['temp'] for hr in forecast])
+                status_line.extend([
+                    StatusText(font, ' {}-{}'.format(low, high)),
+                    StatusText(glyphs, 'o')
+                ])
+                status_lines.append(StatusLine(status_line))
+                status_line = [StatusText(glyphs, '|||||||||||||', COLORS['black'])]
+                temp_range = high - low
+                div = 9 / (temp_range + 0.1)
+                for hr in forecast:
+                    height = round((hr['temp'] - low) * div)
+                    color = COLORS['blue'] if hr['weather'] == 'Rain' else COLORS['green']
+                    color = COLORS['white'] if hr['weather'] == 'Snow' else color
+                    status_line.append(StatusBar(height, color))
+            status_lines.append(StatusLine(status_line))
 
         render(matrix, status_lines)
         time.sleep(5) 
@@ -154,13 +194,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='countdown. find and display subway info')
     parser.add_argument('--glyphs', type=str, help='glyph font', required=True)
     parser.add_argument('--font', type=str, help='normal font', required=True)
+    parser.add_argument('--debug', action='store_true')
     args = parser.parse_args()
+    fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(format=fmt)
+    if args.debug:
+        logger.setLevel('DEBUG')
+        mta.logger.setLevel('DEBUG')
+        weather.logger.setLevel('DEBUG')
+
     glyphs = Font()
     glyphs.LoadFont(args.glyphs)
     font = Font()
     font.LoadFont(args.font)
     render_loop(glyphs, font)
-    #mta.status()
-    #print weather.current()
-    #for f in weather.forecast():
-    #    print f
--- weather.py
+++ weather.py
@@ -1,7 +1,12 @@
-import urllib2
 import json
-import time
 import keys
+import logging
+import os
+import time
+import urllib2
+
+logger = logging.getLogger(__name__)
+CACHE_PATH = '/tmp/forecast_cache.json'
 
 def _extract_weather(obj):
     return {
@@ -10,14 +15,31 @@ def _extract_weather(obj):
         'time': obj['dt'],
     }
 
+def _extract_accuweather(obj):
+    precip = obj.get('PrecipitationProbability', 0) > 50
+    precip_type = 'Rain' if precip else ''
+    if precip:
+        if obj.get('WeatherIcon', 0) in [22, 23, 24, 29, 44]:
+            precip_type = 'Snow'
+
+    return {
+        'weather': precip_type,
+        'temp': int(obj['Temperature']['Value']),
+        'time': obj['EpochDateTime'],
+    }
+
 def make_url(url):
     return url.format(key=keys.WEATHER)
+
+def make_url_accuweather(url):
+    return url.format(key=keys.ACCUWEATHER, loc=keys.ACCUWEATHER_LOCATION)
 
 def current():
     url = make_url('http://api.openweathermap.org/data/2.5/weather?id=5125771&APPID={key}&units=imperial')
     response = urllib2.urlopen(url, None, timeout=4)
     contents = response.read()
     obj = json.loads(contents)
+    logger.debug('current weather\n %s', json.dumps(obj, indent=2))
     return _extract_weather(obj)
 
 def forecast():
@@ -25,9 +47,45 @@ def forecast():
     response = urllib2.urlopen(url, None, timeout=4)
     contents = response.read()
     obj = json.loads(contents)
+    logger.debug('forecast obj\n %s', json.dumps(obj, indent=2))
     forecasts = []
     now = time.time()
     for forecast in obj['list']:
         if forecast['dt'] > now:
             forecasts.append(_extract_weather(forecast))
-    return sorted(forecasts, key=lambda f: f['time'])
+        else:
+            logger.debug('%d (%s) is rejected', forecast['dt'], time.ctime(forecast['dt']))
+    forecasts = sorted(forecasts, key=lambda f: f['time'])
+    logger.debug('forecast weather\n %s', json.dumps(forecasts, indent=2))
+    return forecasts
+
+
+def forecast_accuweather():
+    cache_obj = None
+    obj = None
+    forecasts = []
+    now = time.time()
+    if os.path.exists(CACHE_PATH):
+        with open(CACHE_PATH) as fh:
+            cache_obj = json.loads(fh.read())
+            obj = cache_obj['data']
+        
+    if not cache_obj or cache_obj['expires'] < now:
+        url = make_url_accuweather('https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/{loc}?apikey={key}')
+        logger.debug('accuweather cache miss')
+        response = urllib2.urlopen(url, None, timeout=4)
+        contents = response.read()
+        obj = json.loads(contents)
+        with open(CACHE_PATH, 'w') as fh:
+            fh.write(json.dumps({'expires': int(now) + 60 * 60, 'data': obj}, indent=2))
+
+    for forecast in obj:
+        if forecast['EpochDateTime'] > now:
+            forecasts.append(_extract_accuweather(forecast))
+    
+    forecasts = sorted(forecasts, key=lambda f: f['time'])
+    logger.debug('forecast accuweather\n %s', json.dumps(forecasts, indent=2))
+    return forecasts
+
+
+
